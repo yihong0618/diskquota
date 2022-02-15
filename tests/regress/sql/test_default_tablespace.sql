b386ca6f5ff9fe74bce74fed6daafee8c6430197
liuxueyang <liuxueyang457@gmail.com>
2022-02-15 12:50:12 +0800

    add specified role name

--- tests/regress/sql/test_default_tablespace.sql
+++ tests/regress/sql/test_default_tablespace.sql
@@ -7,9 +7,14 @@
 
 SELECT diskquota.enable_hardlimit();
 
+DROP ROLE if EXISTS role1;
+CREATE ROLE role1 SUPERUSER;
+SET ROLE role1;
+
 DROP TABLE if EXISTS t;
 CREATE TABLE t (i int);
-SELECT diskquota.set_role_tablespace_quota(current_role, 'pg_default', '1 MB');
+
+SELECT diskquota.set_role_tablespace_quota('role1', 'pg_default', '1 MB');
 SELECT diskquota.wait_for_worker_new_epoch();
 -- expect insert to fail
 INSERT INTO t SELECT generate_series(1, 1000000);
@@ -19,10 +24,11 @@ DROP TABLE IF EXISTS t;
 CREATE TABLESPACE custom_tablespace LOCATION '/tmp/custom_tablespace';
 CREATE DATABASE db_with_tablespace TABLESPACE custom_tablespace;
 \c db_with_tablespace;
+SET ROLE role1;
 CREATE EXTENSION diskquota;
 SELECT diskquota.enable_hardlimit();
 
-SELECT diskquota.set_role_tablespace_quota(current_role, 'custom_tablespace', '1 MB');
+SELECT diskquota.set_role_tablespace_quota('role1', 'custom_tablespace', '1 MB');
 SELECT diskquota.wait_for_worker_new_epoch();
 
 -- start_ignore
@@ -40,5 +46,8 @@ DROP EXTENSION IF EXISTS diskquota;
 DROP DATABASE IF EXISTS db_with_tablespace;
 DROP TABLESPACE IF EXISTS custom_tablespace;
 \! rm -rf /tmp/custom_tablespace
+
+RESET ROLE;
+DROP ROLE IF EXISTS role1;
 
 SELECT diskquota.disable_hardlimit();
