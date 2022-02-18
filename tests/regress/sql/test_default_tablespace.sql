f3c19abcbb0a7834eba21f317ee068a59c4add11
liuxueyang <liuxueyang457@gmail.com>
2022-02-18 15:42:13 +0800

    add another role to test hardlimit

--- tests/regress/sql/test_default_tablespace.sql
+++ tests/regress/sql/test_default_tablespace.sql
@@ -6,7 +6,9 @@
 -- end_ignore
 
 DROP ROLE if EXISTS role1;
+DROP ROLE if EXISTS role2;
 CREATE ROLE role1 SUPERUSER;
+CREATE ROLE role2 SUPERUSER;
 SET ROLE role1;
 
 DROP TABLE if EXISTS t;
@@ -28,15 +30,20 @@ WHERE b.tablespace_oid = t.oid AND b.target_oid = r.oid AND r.rolname = 'role1'
 ORDER BY r.rolname, t.spcname, b.target_type;
 
 DROP TABLE IF EXISTS t;
+SELECT diskquota.set_role_tablespace_quota('role1', 'pg_default', '-1');
+
+SET ROLE role2;
 CREATE TABLE t (i int);
 
 -- with hard limits on
 SELECT diskquota.enable_hardlimit();
+SELECT diskquota.set_role_tablespace_quota('role2', 'pg_default', '1 MB');
 SELECT diskquota.wait_for_worker_new_epoch();
--- expect insert to fail
-INSERT INTO t SELECT generate_series(1, 1000000);
+-- expect insert to fail because of hard limits
+INSERT INTO t SELECT generate_series(1, 50000000);
 DROP TABLE IF EXISTS t;
 
+SET ROLE role1;
 -- database in customized tablespace
 CREATE TABLESPACE custom_tablespace LOCATION '/tmp/custom_tablespace';
 CREATE DATABASE db_with_tablespace TABLESPACE custom_tablespace;
@@ -59,13 +66,20 @@ FROM diskquota.blackmap AS b, pg_tablespace AS t, pg_roles AS r
 WHERE b.tablespace_oid = t.oid AND b.target_oid = r.oid AND r.rolname = 'role1'
 ORDER BY r.rolname, t.spcname, b.target_type;
 
+DROP TABLE IF EXISTS t_in_custom_tablespace;
+SELECT diskquota.set_role_tablespace_quota('role1', 'custom_tablespace', '-1');
+SELECT diskquota.wait_for_worker_new_epoch();
+
+SET ROLE role2;
+
 -- with hard limits on
 SELECT diskquota.enable_hardlimit();
+SELECT diskquota.set_role_tablespace_quota('role2', 'custom_tablespace', '1 MB');
 SELECT diskquota.wait_for_worker_new_epoch();
 
 DROP TABLE IF EXISTS t_in_custom_tablespace;
--- expect create table to fail
-CREATE TABLE t_in_custom_tablespace AS SELECT generate_series(1, 1000000);
+-- expect insert to fail because of hard limits
+CREATE TABLE t_in_custom_tablespace AS SELECT generate_series(1, 50000000);
 
 -- clean up
 DROP TABLE IF EXISTS t_in_custom_tablespace;
@@ -77,5 +91,6 @@ DROP TABLESPACE IF EXISTS custom_tablespace;
 
 RESET ROLE;
 DROP ROLE IF EXISTS role1;
+DROP ROLE IF EXISTS role2;
 
 SELECT diskquota.disable_hardlimit();
