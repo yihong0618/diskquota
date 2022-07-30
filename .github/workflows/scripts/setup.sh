f3c467b1a671d4273368f65ecd125297974799d0
yihong0618 <zouzou0208@gmail.com>
2022-07-30 14:19:34 +0800

    f

--- .github/workflows/scripts/setup.sh
+++ .github/workflows/scripts/setup.sh
@@ -38,7 +38,7 @@ ssh_keyscan_for_user() {
 set_limits() {
   # Currently same as what's recommended in install guide
   if [ -d /etc/security/limits.d ]; then
-    cat > /etc/security/limits.d/runner-limits.conf <<-EOF
+    cat > sudo /etc/security/limits.d/runner-limits.conf <<-EOF
                 runner soft core unlimited
                 runner soft nproc 131072
                 runner soft nofile 65536
